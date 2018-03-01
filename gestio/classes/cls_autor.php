0f8e5f0fe704efabd857d34eac91b23d88865b96
David Martínez Gilo <davidmgilo@gmail.com>
2018-03-01 16:53:53 +0100

    first steps

--- gestio/classes/cls_autor.php
+++ gestio/classes/cls_autor.php
@@ -1,8 +1,13 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class autor extends connexio {
+    
+    var $aut_idautor;
+    var $aut_autor;
+    
+    function autor($ruta="../../") {
+        parent::connexio($ruta);
+    }    
+}
 
+?>
--- gestio/classes/cls_connexio.php
+++ gestio/classes/cls_connexio.php
@@ -1,8 +1,13 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class connexio {
+    var $host;
+    var $db;
+    var $user;
+    var $password;
+    var $conn;
+    
+    
+}
 
+?>
