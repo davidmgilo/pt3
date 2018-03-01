fa5295e0607a2d9d19ec84397c429c39c1e75d9d
David Martínez Gilo <davidmgilo@gmail.com>
2018-03-01 17:13:20 +0100

    open-close

--- gestio/classes/cls_connexio.php
+++ gestio/classes/cls_connexio.php
@@ -15,11 +15,26 @@ class connexio {
     }
     
     function DB_Open() {
-        
+        $this->conn = mysqli_connect($this->host, $this->user, $this->passwd);
+        if($this->conn){
+            if(! mysqli_select_db($this->conn, $this->db)){
+                $status = mysqli_error();
+            }else {
+                $status = 0;
+            }
+        } else {
+            $status = mysqli_error();
+        }
+        return ($status);
     }
     
     function DB_Close() {
-        
+        if(mysqli_close($this->conn)){
+            $status=1;
+        } else{
+            $status = 0;
+        }
+        return ($status);
     }
     
     function DB_Select() {
