899cb54a44be3a9634e06bf3bca5fd2a6c17f022
David Martínez Gilo <davidmgilo@gmail.com>
2018-03-01 17:02:23 +0100

    structure connection

--- gestio/classes/cls_connexio.php
+++ gestio/classes/cls_connexio.php
@@ -4,9 +4,35 @@ class connexio {
     var $host;
     var $db;
     var $user;
-    var $password;
+    var $passwd;
     var $conn;
     
+    function connexio($ruta="../../"){
+        $this->host="localhost";
+        $this->db="pt3";
+        $this->user="david";
+        $this->passwd="david";
+    }
+    
+    function DB_Open() {
+        
+    }
+    
+    function DB_Close() {
+        
+    }
+    
+    function DB_Select() {
+        
+    }
+    
+    function DB_Execute() {
+        
+    }
+    
+    function DB_Fetch() {
+        
+    }
     
 }
 
